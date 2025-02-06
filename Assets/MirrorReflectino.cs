5a1e7fa2a733ff312f4a2cc55f958abe1b5e1261
Briann <brianpsss21@gmail.com>
2025-02-06 21:17:20 +0100

    backroom

---
+++ Assets/MirrorReflectino.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorReflectino : MonoBehaviour
+{
+    public Camera mirrorCamera;
+    public Transform mirrorPlane;
+
+    void Start()
+    {
+        if (mirrorCamera == null)
+        {
+            Debug.LogError("Mirror Camera is not assigned!");
+            return;
+        }
+    }
+
+    void LateUpdate()
+    {
+        // Calcular la posición reflejada
+        Vector3 mirrorPosition = mirrorPlane.position;
+        Vector3 cameraPosition = Camera.main.transform.position;
+        Vector3 reflectedPosition = mirrorPosition + Vector3.Reflect(cameraPosition - mirrorPosition, mirrorPlane.up);
+
+        // Ajustar la cámara del espejo
+        mirrorCamera.transform.position = reflectedPosition;
+        mirrorCamera.transform.LookAt(mirrorPlane.position);
+
+        // Renderizar la cámara del espejo
+        mirrorCamera.Render();
+    }
+}
